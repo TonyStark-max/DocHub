d42b524048ce9c609f74768d6165363d92f230d9
TonyStark-max <206331729+TonyStark-max@users.noreply.github.com>
2025-12-17 22:37:44 +0530

    Updated security

--- src/main/java/com/Document/DocHub/Config/SecurityConfig.java
+++ src/main/java/com/Document/DocHub/Config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.Document.DocHub.Config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -23,6 +24,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/doc/**").hasAuthority("USER")
                         .anyRequest().authenticated()
